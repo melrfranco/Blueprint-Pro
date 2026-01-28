9238c6905abb454ecea928cdeae1945d90a054cb
mrfranco91 <156618509+mrfranco91@users.noreply.github.com>
2026-01-28 12:04:36 -0800

    Delete Blueprint-Admin-main directory

--- Blueprint-Admin-main/providers/providerRegistry.ts
+++
@@ -1,9 +0,0 @@
-import { SquareProvider } from "./square/SquareProvider";
-
-export function getSalonProvider(provider: string) {
-  switch (provider) {
-    case "square":
-    default:
-      return SquareProvider;
-  }
-}
--- Blueprint-Admin-main/utils/isSquareCallbackRoute.ts
+++
@@ -1,4 +0,0 @@
-export const isSquareCallbackRoute = () => {
-  if (typeof window === 'undefined') return false;
-  return window.location.pathname === '/square/callback';
-};
