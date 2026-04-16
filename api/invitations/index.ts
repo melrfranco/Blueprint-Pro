5caa9f28d933ab60ffb9454510194d99c000c7ea
melrfranco <mrfranco@asu.edu>
2026-04-15 22:33:09 -0700

    fix: add .js extension for ESM import of token-utils

--- api/invitations/index.ts
+++ api/invitations/index.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { generateRawToken, hashToken, generateClaimCode } from './token-utils';
+import { generateRawToken, hashToken, generateClaimCode } from './token-utils.js';
 import { log } from '../lib/logger';
 
 /**
